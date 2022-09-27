9db8c1425597df130d637897db1e768ba9726776
Andranik Simonian <asimonian@lohika.com>
2022-09-28 01:57:12 +0300

    add deploy script

---
+++ tools/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+RED="\e[1;31m"
+GREEN="\e[1;32m"
+CYAN="\e[0;36m"
+NORMAL="\e[0m"
+DIR_PATH="/var/www/test-task-news/"
+
+echo $CYAN → Remove last build $NORMAL
+rm -rf $DIR_PATH*
+
+echo $CYAN → Copy new build to serve dir $NORMAL
+cp -r ./build/* $DIR_PATH
+
+# echo $CYAN → Deploy app to firebase hosting $NORMAL
+# npx firebase deploy
+
+echo $GREEN → Done $NORMAL
